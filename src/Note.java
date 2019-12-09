e1498fe904522d7f4717b17a1cca4e735f144340
JamesM967 <jamesmosca967@gmail.com>
2019-12-08 21:43:55 -0800

    Add note class and gitignore

---
+++ src/Note.java
@@ -0,0 +1,32 @@
+public class Note {
+
+    private static final int IS_NOTED = 1;
+    private static final int IS_NOT_NOTED = 0;
+
+    private int[] notes;
+
+    public Note(int dimension) {
+        notes = new int[dimension];
+    }
+
+    public boolean hasNote(int notedNumber) {
+        return isInBounds(notedNumber) && notes[notedNumber] == IS_NOTED;
+    }
+
+    public void addNumber(int numToNote) {
+        if (isInBounds(numToNote)) {
+            notes[numToNote] = IS_NOTED;
+        }
+    }
+
+    public void removeNumber(int numToRemove) {
+        if (isInBounds(numToRemove)) {
+            notes[numToRemove] = IS_NOT_NOTED;
+        }
+    }
+
+    private boolean isInBounds(int notedNumber) {
+        return notedNumber > 0 && notedNumber < notes.length;
+    }
+
+}
